ffe44fcf390cde69cabb79ee8132a4475939b516
carriecari <lamklcarrie@gmail.com>
2018-05-30 21:53:19 -0700

    sentiment module added. Changed index.

--- main.js
+++ main.js
@@ -1,10 +1,26 @@
 const express = require('express')
 const path = require('path')
 const PORT = process.env.PORT || 5000
+const app = express()
 
-express()
-  .use(express.static(path.join(__dirname, 'public')))
-  .set('views', path.join(__dirname, 'views'))
-  .set('view engine', 'ejs')
-  .get('/', (req, res) => res.render('index'))
-  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
+
+app.use(express.static(path.join(__dirname, 'public')));
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+app.get('/', function (req, res)  {
+       res.render('index');
+})
+
+app.post('/submit-data', function (req, res) {
+    res.send('POST Request');
+});
+
+app.put('/update-data', function (req, res) {
+    res.send('PUT Request');
+});
+
+app.delete('/delete-data', function (req, res) {
+    res.send('DELETE Request');
+});
+
+app.listen(PORT, () => console.log(`Listening on ${ PORT }`))
