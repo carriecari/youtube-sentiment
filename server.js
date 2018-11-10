c7ab0d1d652c96a9d730a89678f8149215e710f3
carriecari <lamklcarrie@gmail.com>
2018-11-10 15:27:25 -0800

    cleaned server.js

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ var Sentiment = require('sentiment');
 
 app.use(bodyParser.urlencoded({     // to support URL-encoded bodies
     extended: true
-})); 
+}));
 app.use(express.static(path.join(__dirname, 'public')));
 app.set('views', path.join(__dirname, 'views'));
 
@@ -34,39 +34,21 @@ app.get('/about', function (req, res)  {
 });
 
 app.get('/video', function(req, res) {
-    var drinks = [
-        { name: 'Bloody Mary', drunkness: 3 },
-        { name: 'Martini', drunkness: 5 },
-        { name: 'Scotch', drunkness: 10 }
-    ];
-    var tagline = "Any code of your own that you haven't looked at for six or more months might as well have been written by someone else.";
 
     res.render('pages/index', {
-        drinks: drinks,
-        tagline: tagline
+
     });
 });
 
-//app.use('/', indexRouter);
-//
-//app.post('/video'main.parseVideoID);
-//app.all('*', function(req, res, next){
-//  if(req.secure !== true) {
-//    res.redirect('https://'+req.host+req.originalUrl);
-//  } else {
-//    next();
-//  };
-//});
-
 app.post('/', function(req, res){
     var urltext = req.body.video;
-    
+
     main.parseVideoID(req, res).then(function(result) {
         var score = result.score;
         var pos = result.positive;
         var neg = result.negative;
         var comp = (result.comparative).toFixed(4);
-        
+
         res.render('video', {
             videolink: urltext,
             score: score,
